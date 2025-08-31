d7504cc5d03a064b7ef46c9852e4512ee5806fe9
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-08-31 23:49:31 +0600

    make journy cars

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+import JourneySection from '../../components/JourneySection/JourneySection';
 const Home = () => {
     return (
         <div>
           <Hero></Hero>
+          <JourneySection></JourneySection>
         </div>
     );
 };
